7133c8a37dd264548c4ca7433cff66121a56b75f
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2023-12-02 16:03:09 +0100

    seeder format + default admin user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,16 +7,36 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Permission;
 
-class UserSeeder extends Seeder
-{
+class UserSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(): void {
         User::factory()
             ->has(Permission::factory()->count(1))
             ->count(50)
+            ->create();
+
+        User::factory()
+            ->has(
+                Permission::factory()
+                    ->state([
+                        'create_update_post' => 1,
+                        'create_update_reply' => 1,
+                        'delete_post' => 1,
+                        'delete_reply' => 1,
+                        'delete_others_reply' => 1,
+                        'delete_others_post' => 1,
+                        'manage_others' => 1,
+                    ])
+                    ->count(1)
+            )
+            ->state([
+                'name' => 'admin',
+                'email' => 'Admin@noreply.com',
+                'password' => bcrypt('admin'),
+            ])
+            ->count(1)
             ->create();
     }
 }
