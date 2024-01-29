0fa2bc45e59455e719c2d5e59fa619d842a16a0a
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2024-01-29 13:04:14 +0100

    user dash

--- resources/views/userdash.blade.php
+++ resources/views/userdash.blade.php
@@ -1,8 +1,9 @@
 @include('layouts.head')
 @section('Feed', 'title')
 
-<h1>hello admin man!</h1>
-
+@isset($users)
+    
+    
 <div class="flex justify-center">
     <table class="border-black border-2">
         <tr class="bg-gray-400 text-white">
@@ -16,7 +17,6 @@
         @foreach ($users as $key => $user)
             <tr class="{{ $key % 2 == 0 ? '' : 'bg-gray-300' }}">
                 <td class="border-gray-400 border-r-2 p-2">
-                    {{-- <input type="checkbox" name="selected_users[]" value="{{ $user->id }}"> --}}
                     <a class="mr-2 text-gray-200 bg-yellow-500 p-2 rounded-full hover:bg-yellow-400 hover:text-white"
                         href="{{ route('admin@specifyUser', ['id' => $user->id]) }}">Edit</a>
                     <a class="text-gray-200 bg-red-500 p-2 rounded-full hover:bg-red-400 hover:text-white"
@@ -37,84 +37,101 @@
     {{ $users->links('vendor.pagination.tailwind') }}
 </div>
 
-@if (isset($specifiedUser))
-    <form action="" method="POST">
-        @csrf
-        @method('PUT')
-        <div class="flex items-center justify-center">
-            <div class="flex flex-col">
-                <label for="name" class="mb-2">Name:</label>
-                <input type="text" name="name" id="name" value="{{ $specifiedUser->name }}"
-                    class="border border-gray-400 p-2 mb-4">
-            </div>
-            <div class="flex flex-col">
-                <label for="email" class="mb-2">Email:</label>
-                <input type="email" name="email" id="email" value="{{ $specifiedUser->email }}"
-                    class="border border-gray-400 p-2 mb-4">
-            </div>
-            <div class="flex flex-col">
-                <label for="email" class="mb-2">Title:</label>
-                <input type="email" name="email" id="email" value="{{ $specifiedUser->permission->title }}"
-                    class="border border-gray-400 p-2 mb-4">
-            </div>
-            <div class="flex flex-col">
-                <label for="password" class="mb-2">New password:</label>
-                <input type="password" name="password" id="password" class="border border-gray-400 p-2 mb-4">
-            </div>
 
-            <button type="submit"
-                class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">Update</button>
+@endisset
+
+@isset($specifiedUser)
+    <h1>Editing {{ $specifiedUser->name }}, to make a new user go <a href="http://localhost/admin/userdash/">here</a></h1>
+@endisset
+
+<form
+    action="{{ $specifiedUser ? route('admin@updateUser', ['id' => $specifiedUser->id]) : route('admin@updateUser') }}"
+    method="POST">
+    @csrf
+    @method('POST')
+    <div class="flex items-center justify-center">
+        <div class="flex flex-col">
+            <label for="name" class="mb-2" class="hidden">Name:</label>
+            <input type="text" name="name" id="name" placeholder="John Doe"
+                value="{{ $specifiedUser ? $specifiedUser->name : '' }}" class="border border-gray-400 p-2 mb-4">
         </div>
-        <table class="border-black border-2">
-            <tr class="bg-gray-400 text-white">
-                <th class="border-black border-2 w-auto p-2">Permissie</th>
-                <th class="border-black border-2 w-auto p-2">Ja of Nee</th>
-            </tr>
+        <div class="flex flex-col">
+            <label for="email" class="mb-2">Email:</label>
+            <input type="email" name="email" id="email" placeholder="JohnDoe@Example.com"
+                value="{{ $specifiedUser ? $specifiedUser->email : '' }}" class="border border-gray-400 p-2 mb-4">
+        </div>
+        <div class="flex flex-col">
+            <label for="title" class="mb-2">Title:</label>
+            <input type="text" name="title" id="title" placeholder="Regular Blogger"
+                value="{{ $specifiedUser ? $specifiedUser->permission->title : '' }}"
+                class="border border-gray-400 p-2 mb-4">
+        </div>
+        <div class="flex flex-col">
+            <label for="password" class="mb-2">New password:</label>
+            <input type="password" name="password" id="password" placeholder="A strong password"
+                class="border border-gray-400 p-2 mb-4">
+        </div>
 
-            <tr>
-                <th>Eigen posts maken</th>
-                <td class="border-gray-400 border-r-2 p-2">
-                    <input type="checkbox" {{ $specifiedUser->permission->create_update_post ? 'checked' : '' }}>
-                </td>
-            </tr>
-            <tr>
-                <th>Eigen posts verwijderen</th>
-                <td class="border-gray-400 border-r-2 p-2">
-                    <input type="checkbox" {{ $specifiedUser->permission->delete_post ? 'checked' : '' }}>
-                </td>
-            </tr>
-            <tr>
-                <th>Eigen reacties zetten</th>
-                <td class="border-gray-400 border-r-2 p-2">
-                    <input type="checkbox" {{ $specifiedUser->permission->create_update_reply ? 'checked' : '' }}>
-                </td>
-            </tr>
-            <tr>
-                <th>Eigen reacties verwijderen</th>
-                <td class="border-gray-400 border-r-2 p-2">
-                    <input type="checkbox" {{ $specifiedUser->permission->delete_reply ? 'checked' : '' }}>
-                </td>
-            </tr>
-            <tr>
-                <th>Anderen reacties verwijderen</th>
-                <td class="border-gray-400 border-r-2 p-2">
-                    <input type="checkbox" {{ $specifiedUser->permission->delete_others_reply ? 'checked' : '' }}>
-                </td>
-            </tr>
-            <tr>
-                <th>Anderen posts verwijderen</th>
-                <td class="border-gray-400 border-r-2 p-2">
-                    <input type="checkbox" {{ $specifiedUser->permission->delete_others_post ? 'checked' : '' }}>
-                </td>
-            </tr>
-            <tr>
-                <th>Anderen beheren</th>
-                <td class="border-gray-400 border-r-2 p-2">
-                    <input type="checkbox" {{ $specifiedUser->permission->manage_others ? 'checked' : '' }}>
-                </td>
-            </tr>
+    </div>
+    <table class="border-black border-2">
+        <tr class="bg-gray-400 text-white">
+            <th class="border-black border-2 w-auto p-2">Permissie</th>
+            <th class="border-black border-2 w-auto p-2">Ja of Nee</th>
+        </tr>
 
-        </table>
+        <tr>
+            <th>Eigen posts maken</th>
+            <td class="border-gray-400 border-r-2 p-2">
+                <input type="checkbox" name="create_update_post"
+                    {{ $specifiedUser ? ($specifiedUser->permission->create_update_post ? 'checked' : '') : '' }}>
+            </td>
+        </tr>
+        <tr>
+            <th>Eigen posts verwijderen</th>
+            <td class="border-gray-400 border-r-2 p-2">
+                <input type="checkbox" name="delete_post"
+                    {{ $specifiedUser ? ($specifiedUser->permission->delete_post ? 'checked' : '') : '' }}>
+            </td>
+        </tr>
+        <tr>
+            <th>Eigen reacties zetten</th>
+            <td class="border-gray-400 border-r-2 p-2">
+                <input type="checkbox" name="create_update_reply"
+                    {{ $specifiedUser ? ($specifiedUser->permission->create_update_reply ? 'checked' : '') : '' }}>
+            </td>
+        </tr>
+        <tr>
+            <th>Eigen reacties verwijderen</th>
+            <td class="border-gray-400 border-r-2 p-2">
+                <input type="checkbox" name="delete_reply"
+                    {{ $specifiedUser ? ($specifiedUser->permission->delete_reply ? 'checked' : '') : '' }}>
+            </td>
+        </tr>
+        <tr>
+            <th>Anderen reacties verwijderen</th>
+            <td class="border-gray-400 border-r-2 p-2">
+                <input type="checkbox" name="delete_others_reply"
+                    {{ $specifiedUser ? ($specifiedUser->permission->delete_others_reply ? 'checked' : '') : '' }}>
+            </td>
+        </tr>
+        <tr>
+            <th>Anderen posts verwijderen</th>
+            <td class="border-gray-400 border-r-2 p-2">
+                <input type="checkbox" name="delete_others_post"
+                    {{ $specifiedUser ? ($specifiedUser->permission->delete_others_post ? 'checked' : '') : '' }}>
+            </td>
+        </tr>
+        <tr>
+            <th>Anderen beheren</th>
+            <td class="border-gray-400 border-r-2 p-2">
+                <input type="checkbox" name="manage_others"
+                    {{ $specifiedUser ? ($specifiedUser->permission->manage_others ? 'checked' : '') : '' }}>
+            </td>
+        </tr>
 
+    </table>
+
+        <button type="submit" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
+            {{ $specifiedUser ? 'Update' : 'Create' }}
+        </button>
     </form>
-@endif
--- routes/web.php
+++ routes/web.php
@@ -26,48 +26,17 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
     Route::get('/feed', function () {
         return view('feed');
     })->name('feed');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
     Route::get('admin/userdash', 'App\Http\Controllers\AdminController@allowUserDashAccess')->name('admin@checkPermissions');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
     Route::get('admin/userdash/delete/{id}', 'App\Http\Controllers\AdminController@deleteUser')->name('admin@deleteUser');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('admin/userdash/selected/{id}', 'App\Http\Controllers\AdminController@specifyUser')->name('admin@specifyUser');
-});
+    Route::get('admin/userdash/{id}', 'App\Http\Controllers\AdminController@specifyUser')->name('admin@specifyUser');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('admin/userdash/update/{id}', 'App\Http\Controllers\AdminController@updateUser')->name('admin@updateUser');
+    Route::post('admin/userdash/update/{id?}', 'App\Http\Controllers\AdminController@updateUser')->name('admin@updateUser');
 });
-
 Route::get('/test', 'App\Http\Controllers\FeedController@showFeedData');
