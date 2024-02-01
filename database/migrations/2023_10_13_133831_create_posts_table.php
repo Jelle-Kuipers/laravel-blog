e32bbe10268ca24e4621fbc32c7572c6c069b149
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2024-02-01 14:20:10 +0100

    removing score and updating seeders

--- database/migrations/2023_10_13_133831_create_posts_table.php
+++ database/migrations/2023_10_13_133831_create_posts_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->string('title')->default('New Post');
             $table->string('thumbnail_path')->nullable();
             $table->longText('content');
-            $table->float('score', 8, 0)->default(0);
             $table->timestamps();
         });
     }
