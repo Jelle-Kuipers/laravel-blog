247c164e11dc2679d6c6a0a4d14ec80dfd229825
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2024-02-01 12:01:39 +0100

    database changes for missing description field

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,6 +18,7 @@ class PostFactory extends Factory {
             'topic_id' => fake()->numberBetween(1, 10),
             'user_id' => fake()->numberBetween(1, 10),
             'title' => fake()->sentence(1),
+            'description' => fake()->sentence(2),
             'thumbnail_path' => 'https://source.unsplash.com/random/800x600',
             'content' => fake()->sentence(3),
             'score' => fake()->randomFloat(2, 0, 100),
