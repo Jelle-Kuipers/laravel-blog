30fa2a9398e6797c39bd06bea113c63b21b5fd7f
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2024-02-02 13:17:21 +0100

    spellfix

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -4,7 +4,7 @@
 <div class="h-full w-full items-center justify-center grid grid-cols-2 gap-6">
     <div class="flex flex-col h-3/4 items-center justify-around py-20">
         <h1 class="text-6xl text-purple-600">Welcome to:</h1>
-            <h1 class="text-8xl text-purple-800">FictiousForums</h1>
+            <h1 class="text-8xl text-purple-800">FictitiousForums</h1>
         <h2 class="text-4xl text-purple-500">Start reading and start blogging!</h2>
     </div>
     <div class="flex flex-col h-3/4 items-center justify-around py-20">
